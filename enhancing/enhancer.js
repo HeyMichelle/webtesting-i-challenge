4e764efc7953a3ff11ec695a621a914b16fa7ab3
HeyMichelle <miscmiche@gmail.com>
2020-09-08 19:59:50 -0400

    chore: completed cz set up refactor: deleted unneccessary files from initial set up

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,68 @@
 module.exports = {
-  success,
+  succeed,
   fail,
   repair,
   get,
 };
 
-function success(item) {
-  return { ...item };
+function succeed(item) {
+  if (item.enhancement == 20) {
+    return item
+  } else {
+    return {
+      ...item,
+      enhancement: item.enhancement + 1 
+    }
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return {
+      ...item,
+      durability: item.durability - 5
+    }
+  } 
+
+  if (item.enhancement > 16) {
+    return {
+      ...item,
+      enhancement: item.enhancement - 1
+    }
+  } else if (item.enhancement > 15) {
+      return {
+        ...item,
+        durability: item.durability - 10
+      }
+  } 
 }
 
 function repair(item) {
-  return { ...item };
+  return {
+    ...item, 
+    durability: 100
+    // or item.durability = 100
+  }
 }
 
 function get(item) {
-  return { ...item };
+  if (item.enhancement == 0) {
+    return {
+      ...item,
+      name: item.name
+    }
+  }
+
+  if (item.enhancement > 0) {
+    return {
+      ...item,
+      name: `[+${item.enhancement}] ${item.name}`
+    }
+  }
 }
+
+// - Add a `get()` method to the `enhancer` object that takes an `item` and **returns a new item** with the `name` property modified according to the following rules:
+//   - if the enhancement level is 0, the the name is not modified.
+//   - if the enhancement level is greater than 0, change the name to include the enhancement level, preceded by a plus sign ( + ), between square brackets before the item's name. Example: the name of a "Iron Sword" enhanced to 7 would be "[+7] Iron Sword".
+// - Design and build a Web Application to test the enhancing module you implemented for the MVP.
+// - Work on [this repository for extra practice testing JavaScript Functions](https://github.com/LambdaSchool/Testing).
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,109 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+describe('enhancer functions', () => {
+  describe('repair', () => {
+    let testItem = {
+      name: 'item 1',
+      enhancement: 15,
+      durability: 75
+    }
+    let result;
+    let expected = 100;
+    it('update durability to 100', () => {
+      result = enhancer.repair(testItem)
+      expect(result.durability).toBe(expected)
+    })
+  })
+
+  describe('succeed', () => {
+    let testItem2 = {
+      name: 'item 2',
+      enhancement: 8,
+      durability: 65
+    }
+    let result;
+    let expected = testItem2.enhancement + 1
+    it('increase enhancement by 1', () => {
+      result = enhancer.succeed(testItem2)
+      expect(result.enhancement).toBe(expected)
+    })
+
+    it('if enhancement = 20, no change', () => {
+      let testItem = {
+        name: 'item 3',
+        enhancement: 20,
+        durability: 98
+      }
+      let result;
+      let expected = testItem.enhancement
+      result = enhancer.succeed(testItem)
+      expect(result.enhancement).toBe(expected)
+    })
+  })
+
+  describe('fail', () => {
+    it('if enhancement < 15, durability - 5', () => {
+      let testItem = {
+        name: 'testItem 4',
+        enhancement: 14,
+        durability: 75
+      }
+      let result;
+      let expected = testItem.durability - 5
+      result = enhancer.fail(testItem)
+      expect(result.durability).toBe(expected)
+    })
+
+    it('if enhancement > 15, durability - 10', () => {
+      let testItem = {
+        name: 'testItem 45',
+        enhancement: 16,
+        durability: 95
+      }
+      let result;
+      let expected = testItem.durability - 10
+      result = enhancer.fail(testItem)
+      expect(result.durability).toBe(expected)
+    })
+
+    it('if enhancement > 16, enhancement - 1', () => {
+      let testItem = {
+        name: 'testItem 7',
+        enhancement: 19,
+        durability: 65
+      }
+      let result;
+      let expected = testItem.enhancement - 1
+      result = enhancer.fail(testItem)
+      expect(result.enhancement).toBe(expected)
+    })
+
+    describe('get', () => {
+      it('if enhancement = 0, no name change', () => {
+        let testItem = {
+          name: 'test item 92',
+          enhancement: 0,
+          durability: 42
+        }
+          let result;
+          let expected = testItem.name 
+          result = enhancer.get(testItem)
+          expect(result.name).toBe(expected)
+      })
+
+      it('if enhancement > 0, name = `[+ enhancement lvl] item.name` ', () => {
+        let testItem = {
+          name: 'fire blaze og sword op',
+          enhancement: 19,
+          durability: 98
+        }
+        let result;
+        let expected = `[+${testItem.enhancement}] ${testItem.name}`
+        result = enhancer.get(testItem)
+        expect(result.name).toBe(expected)
+      })
+    })
+  })
+})
